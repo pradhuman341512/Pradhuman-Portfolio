5a24a33703f9f3324f1bb7aae7c117da07e25a9e
Pradhuman Singh <singhpradhuman077@gmail.com>
2025-06-09 22:58:55 +0530

    updadte data

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,24 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Hello! I'm <strong>Pradhuman Singh</strong>, a creative and results-driven <strong>Full Stack Developer</strong> with a strong foundation in both frontend and backend technologies. I transform ideas into scalable, modern web applications that are not only functional but also user-friendly.
+          </p>
+          <p className="hero--section-description">
+            With hands-on experience in <strong>React.js, Next.js, Node.js, MongoDB, MySQL, Appwrite</strong> and <strong>Python</strong>, I’ve built dynamic platforms like a real estate management system, a healthy food delivery app, and an AI-powered candidate evaluator. I’ve also worked on real-world data analytics projects during my internship, learning to turn raw data into actionable insights.
+          </p>
+          <p className="hero--section-description">
+            I’m a fast learner, team collaborator, and clean code enthusiast who loves building full-stack solutions that solve real problems. Whether it’s enhancing user experience, architecting efficient backend systems, or working with APIs and modern libraries, I take pride in delivering quality work with passion and purpose.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,25 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Pradhuman</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+  I am a passionate Full Stack Developer with expertise in building scalable web applications using modern technologies like React, NextJs, JS, Node.js, Express, MongoDb, Postgress, UI/UX, Figma, Python, Prisma and Docker. 
+  <br /> I love transforming complex problems into elegant, user-friendly solutions while continuously learning and adapting to new challenges.,
+</p>
+
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
